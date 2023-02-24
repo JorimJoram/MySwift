e8d6536dec76349a855ab7c5611a9b74b0a86802
wkdgyfla97 <wkdgyfla97@gmail.com>
2023-02-24 09:23:09 +0900

    Initial Commit

---
+++ MySwift/MySwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftApp.swift
+//  MySwift
+//
+//  Created by 장효림 on 2023/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
